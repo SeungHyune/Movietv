43cd75306e805fb7ec9ba44d9b71ede5eb8d485a
seunghyun <tmdgus7820@naver.com>
2023-12-23 12:15:37 +0900

    [feat] : Header 컴포넌트 생성

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,14 @@
+import Search from '@/components/common/Search';
+import Theme from '@/components/common/Theme';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>MovieTV</h1>
+      <Search />
+      <Theme />
+    </header>
+  );
+};
+
+export default Header;
