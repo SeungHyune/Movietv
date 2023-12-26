675e948884e53941a76113c06d5ee159523f364e
seunghyun <tmdgus7820@naver.com>
2023-12-26 23:51:36 +0900

    [design] : font 설정 reset css 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import Header from '@/components/layout/Header';
 import MovieList from './components/common/movie/MovieList';
+import './styles/reset.css';
 
 function App() {
   return (
