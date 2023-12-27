3ed5401e6a324434d5691f44b809adccd5c07188
seunghyun <tmdgus7820@naver.com>
2023-12-27 21:12:43 +0900

    [design] emotion 설치 및 header css 완료

--- src/components/common/Theme/index.tsx
+++ src/components/common/Theme/index.tsx
@@ -1,5 +1,26 @@
 import { useRecoilState } from 'recoil';
 import { movieAtom } from '@/atoms/movie';
+import styled from '@emotion/styled';
+
+const ButtonTheme = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  background-color: #0000000d;
+  border-radius: 50%;
+
+  & img {
+    width: 16px;
+    height: 16px;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: rotate(180deg);
+    }
+  }
+`;
 
 const Theme = () => {
   const [movieState, setMovieState] = useRecoilState(movieAtom);
@@ -12,7 +33,7 @@ const Theme = () => {
   };
 
   return (
-    <button onClick={onThemeChange}>
+    <ButtonTheme onClick={onThemeChange}>
       <img
         src={
           movieState.isTheme
@@ -20,7 +41,7 @@ const Theme = () => {
             : 'https://github.com/SeungHyune/vue-movie/blob/main/image/theme/dark-icon.png?raw=true'
         }
       />
-    </button>
+    </ButtonTheme>
   );
 };
 
