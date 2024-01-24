44e32f7774cca5fb5ea892cbf20eaf09d204d1e2
seunghyun <tmdgus7820@naver.com>
2024-01-24 23:45:33 +0900

    [design] 전체 레이아웃 너비 수정

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,40 @@
+interface ButtonProps {
+  text: string;
+  width?: number;
+  height?: number;
+  radius: string;
+  backgroundColor: string;
+  color: string;
+  fontWeight?: string;
+  onClick: () => void;
+}
+
+const Button = ({
+  text,
+  width = 200,
+  height = 50,
+  radius,
+  backgroundColor,
+  color,
+  fontWeight = 'normal',
+  ...props
+}: ButtonProps) => {
+  const buttonStyle = {
+    width: width,
+    height: height,
+    borderRadius: `${radius}`,
+    backgroundColor,
+    color,
+    fontWeight
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      {...props}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
