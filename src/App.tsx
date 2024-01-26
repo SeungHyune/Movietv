46d467294a35f5ab427463d5329c698a2b0dfbe4
seunghyun <tmdgus7820@naver.com>
2024-01-27 00:29:00 +0900

    [style] css 파일 scss 파일로 변경 및 Dark 모드 적용 완료

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/layout/Header';
 import Home from '@/components/layout/Home';
 import MovieList from '@/components/movie/MovieList';
 import MovieInfo from '@/components/movie/MovieInfo';
-import '@/styles/main.css';
+import '@/styles/main.scss';
 
 function App() {
   const [movieState] = useRecoilState(movieAtom);
