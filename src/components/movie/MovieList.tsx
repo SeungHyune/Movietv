e4219557b448766cc62164f329b608fd2338c41b
seunghyun <tmdgus7820@naver.com>
2024-01-28 22:12:31 +0900

    [style] : MovieList 페이지 Styled Components로 수정

--- src/components/movie/MovieList.tsx
+++ src/components/movie/MovieList.tsx
@@ -1,6 +1,7 @@
 import { movieAtom } from '@/atoms/movie';
 import { useRecoilState } from 'recoil';
 import { fetchMovieList } from '@/api/movie';
+import styled from '@emotion/styled';
 import MovieItem from './MovieItem';
 import MovieTotalResult from './MovieTotalResult';
 
@@ -26,7 +27,7 @@ const MovieList = () => {
   };
 
   return (
-    <div className='movielist-container'>
+    <MovieListContainer>
       {movieState.totalResults ? <MovieTotalResult /> : null}
       <ul>
         {movieList.map((movie) => (
@@ -44,8 +45,86 @@ const MovieList = () => {
           More
         </button>
       ) : null}
-    </div>
+    </MovieListContainer>
   );
 };
+
+const MovieListContainer = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  ul {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: repeat(5, 1fr);
+    row-gap: 20px;
+    column-gap: 20px;
+    li {
+      position: relative;
+      list-style: none;
+      cursor: pointer;
+      &:hover {
+        &::before {
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          border-radius: 5px;
+          content: '';
+          background-color: rgba(0, 0, 0, 0.5);
+          z-index: 10;
+        }
+
+        img {
+          transform: scale(1.03);
+        }
+
+        .movie-info {
+          opacity: 1;
+        }
+      }
+
+      .movie-poster {
+        height: 100%;
+        overflow: hidden;
+        border-radius: 5px;
+
+        img {
+          max-width: 100%;
+          height: 100%;
+          object-fit: cover;
+          transition: transform 0.2s;
+          border-radius: 5px;
+        }
+      }
+
+      .movie-info {
+        opacity: 0;
+        position: absolute;
+        width: calc(100% - 20px);
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        transition: opacity 0.2s;
+        color: #fff;
+        z-index: 20;
+        text-align: center;
+
+        strong {
+          display: -webkit-box;
+          font-weight: 600;
+          margin-bottom: 10px;
+          white-space: normal;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          padding: 2px 0;
+          line-height: 1.2;
+          font-size: 20px;
+        }
+      }
+    }
+  }
+`;
 
 export default MovieList;
