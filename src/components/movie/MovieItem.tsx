43b7559d9e6a683d0c421e3f9689368dfbba74f5
seunghyun <tmdgus7820@naver.com>
2024-02-04 23:51:35 +0900

    [style] : 이미지 없는 경우 처리(기본 이미지 추가)

--- src/components/movie/MovieItem.tsx
+++ src/components/movie/MovieItem.tsx
@@ -20,7 +20,13 @@ const MovieItem = React.forwardRef<HTMLLIElement, MovieItemProps>(
         ref={ref}
       >
         <div className='movie-poster'>
-          <img src={movie.Poster} />
+          <img
+            src={
+              movie.Poster === 'N/A'
+                ? 'https://placehold.co/250x330?text=No+Image'
+                : movie.Poster
+            }
+          />
         </div>
         <div className='movie-info'>
           <strong>{movie.Title}</strong>
