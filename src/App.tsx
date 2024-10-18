d65c481b5fe3949013273904bb61b61f10cb5088
seunghyun <tmdgus7820@naver.com>
2024-10-18 14:27:34 +0900

    [refactor] : 6.4 router 방식 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { movieAtom } from './atoms/movie';
-import styled from '@emotion/styled';
+import { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
 import '@/styles/main.scss';
-import Header from '@/components/layout/Header';
 
-const Home = React.lazy(() => import('@/components/layout/Home'));
-const MovieList = React.lazy(() => import('@/components/movie/MovieList'));
-const MovieInfo = React.lazy(() => import('@/components/movie/MovieInfo'));
+import router from './router/router';
 
 function App() {
-  const [movieState] = useRecoilState(movieAtom);
   return (
     <Suspense>
-      <BrowserRouter>
-        <div className={`wrapper ${movieState.isDark ? 'dark' : 'light'}`}>
-          <Header />
-          <Main>
-            <div className='main-wrap'>
-              <Routes>
-                <Route
-                  path='/'
-                  element={<Home />}
-                />
-                <Route
-                  path='/movie/:movieTitle'
-                  element={<MovieList />}
-                />
-                <Route
-                  path='/movie/:movieTitle/:id'
-                  element={<MovieInfo />}
-                />
-              </Routes>
-            </div>
-          </Main>
-        </div>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Suspense>
   );
 }
-
-const Main = styled.main`
-  padding-top: 80px;
-  min-height: 100vh;
-
-  @media (max-width: 380px) {
-    padding-top: 120px;
-  }
-`;
 
 export default App;
