5d546bdb733d60fc539467f6b8017702ff31d3f2
seunghyun <tmdgus7820@naver.com>
2024-10-19 22:51:02 +0900

    [feat] : tanstack query devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 
 import App from './App.tsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RecoilRoot>
         <App />
       </RecoilRoot>
